2d70d9936d746399950b6e3b4b172f99aa401cd5
YJ Lee <leeyuejia@gmail.com>
2020-07-22 17:38:48 +0800

    Updates

---
+++ precache-manifest.4a3c0711273e490ed293a65a819007f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ce41ce3ee1e96b3bd32e732fde8715",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "11bff1e35ddb3ecfb9d8",
+    "url": "/movie_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4bc9b7aafe10c0b45a91",
+    "url": "/movie_app/static/js/2.a6cff7c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.a6cff7c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11bff1e35ddb3ecfb9d8",
+    "url": "/movie_app/static/js/main.42ede75f.chunk.js"
+  },
+  {
+    "revision": "53edcb5fd04563502f4d",
+    "url": "/movie_app/static/js/runtime-main.66d27091.js"
+  }
+]);
